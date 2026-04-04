1132a25cf38539529ab942b8459fed3c1789c823
hdelar824-alt <hdelar824@gmail.com>
2026-04-04 15:00:41 -0500

    Add files via upload

---
+++ once_upon_an_arduino.ino
@@ -0,0 +1,20 @@
+#include <Servo.h>
+
+Servo servo_hades;
+const int buttonPin = 2; 
+
+void setup() {
+  servo_hades.attach(9); 
+  pinMode(buttonPin, INPUT);
+}
+
+void loop() {
+  //flip up hades 
+  if (digitalRead(buttonPin) == HIGH) {
+    servo_hades.write(90); //move
+  } else {
+    servo_hades.write(0); //not move
+  }
+
+
+}
